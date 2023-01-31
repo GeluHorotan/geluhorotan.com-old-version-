d49063d82193e99c05a65eac7b669f183af2ac8f
Gelu Horotan <100289634+GeluHorotan@users.noreply.github.com>
2023-01-31 22:58:14 +0200

    feat: adding styling to error state in form validation

--- src/components/form/Input.tsx
+++ src/components/form/Input.tsx
@@ -1,4 +1,6 @@
+import { motion, useAnimationControls } from 'framer-motion';
 import type { ReactNode } from 'react';
+import { useEffect } from 'react';
 
 import {
   Tooltip,
@@ -36,14 +38,27 @@ const Input = ({
   backgroundColor,
   icon,
 }: Props) => {
+  const controls = useAnimationControls();
+
+  useEffect(() => {
+    if (error) {
+      controls.start({ x: [0, 5, 0, 5, 0] });
+    }
+  }, [error]);
+
   return (
     <TooltipProvider>
       <Tooltip>
-        <div className="relative flex flex-col gap-1  ">
+        <motion.div
+          initial={{ x: 0 }}
+          animate={controls}
+          transition={{ type: 'spring', duration: 0.1 }}
+          className="relative flex flex-col gap-1  "
+        >
           <label
             htmlFor={name}
-            className={`${
-              labelColor || 'text-secondary '
+            className={`${labelColor || 'text-secondary'} ${
+              !error || 'text-error'
             } flex items-center gap-0.5`}
           >
             {label}
@@ -58,16 +73,20 @@ const Input = ({
                 id={id}
                 className={`peer relative w-full rounded-lg ${backgroundColor} py-2  ${
                   icon ? 'indent-8' : 'px-4'
-                } outline-none transition-all  duration-200  ease-in-out `}
+                }  outline-none ${
+                  error ? 'border border-error' : ''
+                }  transition-all  duration-100 ease-in-out `}
                 value={value}
                 onChange={onChangeHandler}
                 onBlur={onBlurHandler}
               />
             </TooltipTrigger>
 
-            <TooltipContent className="bg-primary text-secondary">
-              <p>{error}</p>
-            </TooltipContent>
+            {error && (
+              <TooltipContent className="bg-primary text-secondary">
+                <p>{error}</p>
+              </TooltipContent>
+            )}
 
             {icon && (
               <div className=" absolute top-2/4 left-2 -translate-y-2/4 ">
@@ -75,7 +94,7 @@ const Input = ({
               </div>
             )}
           </div>
-        </div>
+        </motion.div>
       </Tooltip>
     </TooltipProvider>
   );
