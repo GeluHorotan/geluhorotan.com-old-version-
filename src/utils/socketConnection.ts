82bc5111d640562a5af6d664156cb3ca52db71db
Gelu Horotan <100289634+GeluHorotan@users.noreply.github.com>
2023-02-13 15:24:06 +0200

    test: changing the ws url

--- src/utils/socketConnection.ts
+++ src/utils/socketConnection.ts
@@ -1,5 +1,5 @@
-const socketDevEndpoint = 'http://localhost:8080';
-const socketProdEndpoint = 'https://geluhorotancom-horotangelu17.b4a.run:8080';
+const socketDevEndpoint = 'http://localhost:5000';
+const socketProdEndpoint = 'https://geluhorotancom-horotangelu17.b4a.run';
 
 export const socketEndpoint =
   process.env.NODE_ENV === 'production'
