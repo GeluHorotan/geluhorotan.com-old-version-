72db91b0200d3da6195c6312ea354cff5c843447
Gelu Horotan <100289634+GeluHorotan@users.noreply.github.com>
2023-02-21 15:39:50 +0200

    feat: implementing media query hook

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { HiOutlineChevronRight } from 'react-icons/hi';
 
 import { useAuth } from '@/context/hooks/useAuth';
+import useMediaQuery from '@/customHooks/useMediaQuery';
 
 import DarkMode from './DarkMode';
 import Dropdown from './Dropdown';
@@ -12,6 +13,7 @@ import Logo from './svgs/Logo';
 
 const Navigation = () => {
   const { isAuthenticated, user, logout } = useAuth();
+  const isMobile = useMediaQuery('(max-width: 768px)');
 
   const navItems = [
     { id: 1, name: 'home' },
@@ -52,49 +54,51 @@ const Navigation = () => {
 
   return (
     <>
-      <nav
-        className={
-          ' flex w-full  flex-row items-center justify-between   bg-primary py-3 px-14   dark:bg-secondary'
-        }
-      >
-        <ul className="flex w-1/2 flex-row items-center justify-start gap-16  ">
-          <Logo size={36} />
-          <div className="flex items-center justify-center gap-8">
-            {navItems?.map((item) => {
-              return (
-                <li key={item.id} className="navigation-item ">
-                  <Link href={`/${item.name === 'home' ? '' : item.name}`}>
-                    {item.name}
-                  </Link>
-                </li>
-              );
-            })}
-          </div>
-        </ul>
-        <ul className="flex items-center  justify-between gap-12 ">
-          {!isAuthenticated ? (
-            <li className="navigation-item ">
-              {' '}
-              <Link href="/signin">SIGN IN</Link>
-            </li>
-          ) : (
-            <Dropdown data={dropdownData} secondLabel={user?.email}>
-              <div className="flex  items-center justify-between gap-6 ">
-                <div className="flex  items-center justify-between gap-3 ">
-                  <ProfilePicture size="small" />
-                  <span className="text-secondary dark:text-primary">
-                    {user?.firstName}&nbsp;{user?.lastName}
-                  </span>
+      {!isMobile && (
+        <nav
+          className={
+            ' flex w-full  flex-row items-center justify-between   bg-primary py-3 px-14   dark:bg-secondary'
+          }
+        >
+          <ul className="flex w-1/2 flex-row items-center justify-start gap-16  ">
+            <Logo size={36} />
+            <div className="flex items-center justify-center gap-8">
+              {navItems?.map((item) => {
+                return (
+                  <li key={item.id} className="navigation-item ">
+                    <Link href={`/${item.name === 'home' ? '' : item.name}`}>
+                      {item.name}
+                    </Link>
+                  </li>
+                );
+              })}
+            </div>
+          </ul>
+          <ul className="flex items-center  justify-between gap-12 ">
+            {!isAuthenticated ? (
+              <li className="navigation-item ">
+                {' '}
+                <Link href="/signin">SIGN IN</Link>
+              </li>
+            ) : (
+              <Dropdown data={dropdownData} secondLabel={user?.email}>
+                <div className="flex  items-center justify-between gap-6 ">
+                  <div className="flex  items-center justify-between gap-3 ">
+                    <ProfilePicture size="small" />
+                    <span className="text-secondary dark:text-primary">
+                      {user?.firstName}&nbsp;{user?.lastName}
+                    </span>
+                  </div>
+                  <HiOutlineChevronRight className="rotate-90 text-secondary dark:text-primary"></HiOutlineChevronRight>
                 </div>
-                <HiOutlineChevronRight className="rotate-90 text-secondary dark:text-primary"></HiOutlineChevronRight>
-              </div>
-            </Dropdown>
-          )}
-          <DarkMode />
-        </ul>
-      </nav>
+              </Dropdown>
+            )}
+            <DarkMode />
+          </ul>
+        </nav>
+      )}
 
-      <Sidebar />
+      {isMobile && <Sidebar />}
     </>
   );
 };
