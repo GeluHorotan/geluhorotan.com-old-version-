eb380d8f6453f630ad858418a34fa21843501837
Gelu Horotan <100289634+GeluHorotan@users.noreply.github.com>
2023-03-14 21:13:57 +0200

    fix: merge conflict

---
+++ src/components/SidebarToggler.tsx
@@ -0,0 +1,84 @@
+import type { SVGMotionProps } from 'framer-motion';
+import { motion } from 'framer-motion';
+import type { FC } from 'react';
+import React, { useEffect, useState } from 'react';
+
+import type { Theme } from '@/customHooks/useDarkMode';
+
+const Path = (
+  props: JSX.IntrinsicAttributes &
+    SVGMotionProps<SVGPathElement> &
+    React.RefAttributes<SVGPathElement>
+) => (
+  <motion.path
+    fill="transparent"
+    strokeWidth="3"
+    strokeLinecap="round"
+    {...props}
+  />
+);
+
+interface SidebarTogglerProps {
+  toggle?: any;
+  isOpen: boolean;
+  theme: Theme;
+  setTheme: React.Dispatch<React.SetStateAction<Theme>>;
+}
+
+const SidebarToggler: FC<SidebarTogglerProps> = ({
+  toggle,
+  isOpen,
+  theme,
+  setTheme,
+}) => {
+  const [isDarkMode, setIsDarkMode] = useState(
+    typeof window !== 'undefined' && localStorage.getItem('theme') === 'dark'
+  );
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      setIsDarkMode(false);
+    } else {
+      setIsDarkMode(true);
+    }
+  }, [theme]);
+
+  const pathClassNames = isDarkMode ? 'stroke-primary' : 'stroke-secondary';
+
+  return (
+    <button
+      onClick={() => toggle()}
+      className={`
+     h-12  w-12 cursor-pointer  rounded-full border-none   outline-none
+  `}
+    >
+      <svg width="23" height="23" viewBox="0 0 23 23">
+        <Path
+          variants={{
+            closed: { d: 'M 2 2.5 L 20 2.5' },
+            open: { d: 'M 3 16.5 L 17 2.5' },
+          }}
+          className={`${pathClassNames} transition-colors duration-700`}
+        />
+        <Path
+          d="M 2 9.423 L 20 9.423"
+          variants={{
+            closed: { opacity: 1 },
+            open: { opacity: 0 },
+          }}
+          transition={{ duration: 0.1 }}
+          className={`${pathClassNames} transition-colors duration-700`}
+        />
+        <Path
+          variants={{
+            closed: { d: 'M 2 16.346 L 20 16.346' },
+            open: { d: 'M 3 2.5 L 17 16.346' },
+          }}
+          className={`${pathClassNames} transition-colors duration-700`}
+        />
+      </svg>
+    </button>
+  );
+};
+
+export default SidebarToggler;
