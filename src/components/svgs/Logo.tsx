fdec70b7d2b792edcfe742365cbcaf196423d1e8
GeluHorotan <100289634+GeluHorotan@users.noreply.github.com>
2023-06-24 16:39:18 +0300

    feat: 404 not found page

--- src/components/svgs/Logo.tsx
+++ src/components/svgs/Logo.tsx
@@ -23,21 +23,21 @@ const Logo = ({ size, className, primaryColor, secondaryColor }: Props) => {
           data-name="Path 2"
           d="M202.568,244.758l104.277,60.2-39.834,23h0L135.3,404.011l67.268,38.832L381.907,339.3l.008-68.676L202.568,167.07Z"
           transform="translate(-23.228 -28.683)"
-          className={`${primaryColor}`}
+          className={`${primaryColor || 'fill-accent dark:fill-accent2'}`}
         />
         <path
           id="Path_3"
           data-name="Path 3"
           d="M199.994,0,120.31,46v77.672l50.271-29.024L199.994,77.68,379.342,181.212V103.54Z"
           transform="translate(-20.655)"
-          className={`${secondaryColor}`}
+          className={`${secondaryColor || 'fill-accent2 dark:fill-accent'}`}
         />
         <path
           id="Path_4"
           data-name="Path 4"
           d="M67.276,242.349V78.11L0,116.95V324.03l79.684,46,67.268-38.84-79.676-46Z"
           transform="translate(0 -13.41)"
-          className={`${primaryColor}`}
+          className={`${primaryColor || 'fill-accent dark:fill-accent2'}`}
         />
       </g>
     </svg>
---
+++ src/pages/404.tsx
@@ -0,0 +1,61 @@
+import type { NextPage } from 'next';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
+import Logo from '@/components/svgs/Logo';
+import { Meta } from '@/layouts/Meta';
+import { Main } from '@/templates/Main';
+
+type Props = {
+  children?: React.ReactNode;
+};
+
+const NotFoundPage: NextPage<Props> = () => {
+  const router = useRouter();
+  const [cooldown, setCooldown] = useState<number>(10);
+
+  useEffect(() => {
+    const countdown = setInterval(() => {
+      setCooldown((prevCooldown) => prevCooldown - 1);
+    }, 1000);
+
+    if (cooldown === 0) {
+      clearInterval(countdown);
+      router.push('/');
+    }
+
+    return () => clearInterval(countdown);
+  }, [cooldown, router]);
+
+  return (
+    <Main
+      meta={
+        <Meta
+          title="Gelu Horotan - Homepage"
+          description={`Oops! The page you are looking for could not be found. Don't worry, we'll redirect you back to the homepage shortly. Stay tuned!`}
+        />
+      }
+    >
+      <div className="flex h-screen flex-col items-center justify-center gap-14 px-20 text-primary dark:text-secondary max-md:px-4">
+        <Logo size={200} />
+        <div className="flex flex-col items-center justify-center gap-4 ">
+          <h1 className="  text-center font-bold ">Page Not Found</h1>
+
+          <p className=" text-lg">
+            Redirecting back to the homepage in {cooldown} seconds...
+          </p>
+        </div>
+        <Link
+          href="/"
+          className=" flex 
+               w-max cursor-pointer items-center rounded-xl border-2 border-accent px-4 py-2 text-primary  dark:border-accent2 dark:text-secondary"
+        >
+          BACK TO HOMEPAGE
+        </Link>
+      </div>
+    </Main>
+  );
+};
+
+export default NotFoundPage;
